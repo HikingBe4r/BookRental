68a32d077cbdba5fe61f96945f23dc9c50cbe5de
kodica0308 <kodica0308@kodica0308-PC>
2017-04-12 14:10:47 +0900

    프린트다섯줄수행완료.

--- src/gitTest1.java
+++ src/gitTest1.java
@@ -1,6 +1,10 @@
 
 public class gitTest1 {
 	public static void main(String[] args) {
-		System.out.println("ºÏ·»Å» ¾È³ç");
+		System.out.println("1");
+		System.out.println("1");
+		System.out.println("1");
+		System.out.println("1");
+		System.out.println("1");
 	}
 }

af82214558665f0d5b7033c359ca8339e6a74424
HikingBe4r <kimisa0927@gmail.com>
2017-04-12 12:29:12 +0900

    DBconn 연결성공

---
+++ src/conn/DBconn.java
@@ -0,0 +1,31 @@
+package conn;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBconn {
+
+	public static Connection getConnection() throws SQLException {
+		// driver 설정
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+
+		} catch (ClassNotFoundException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+
+		// Connection 객체 생성
+
+		String url = "jdbc:oracle:thin:@kodica0303-PC:1521:xe";
+		String user = "rental_server";
+		String password = "java1234";
+
+		Connection conn = DriverManager.getConnection(url, user, password);
+		
+		System.out.println("연결 성공");
+		
+		return conn;
+	}
+}

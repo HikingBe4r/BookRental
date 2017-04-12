4ccd58d84b6de09056bb1ed21a0ecb1bd0e0dd79
kodica0308 <kodica0308@kodica0308-PC>
2017-04-12 14:12:37 +0900

    ;.;'.

--- src/gitTest1.java
+++ src/gitTest1.java
@@ -3,8 +3,5 @@ public class gitTest1 {
 	public static void main(String[] args) {
 		System.out.println("1");
 		System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");
 	}
 }

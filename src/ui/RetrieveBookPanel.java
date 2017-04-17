ac5a5f3a1a7330c11f8687d9f0a8f319c4691d14
kodica0309 <kodica0309@kodica0309-PC>
2017-04-17 19:17:00 +0900

    도서 관리 수정 170417

--- src/ui/RetrieveBookPanel.java
+++ src/ui/RetrieveBookPanel.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
@@ -34,15 +35,15 @@ import domain.BookVO;
 import domain.GenreVO;
 
 public class RetrieveBookPanel extends JPanel implements ActionListener {
-   private JTextField textField;
-   private JTextField textField_1;
-   private JTextField textField_2;
-   private JTextField textField_3;
-   private JTextField textField_4;
-   private JTextField textField_5;
+   private JTextField subjectTF;
+   private JTextField writerTF;
+   private JTextField publisherTF;
+   private JTextField publishDateTF;
+   private JTextField isbnTF;
+   private JTextField textTF;
    private JTable table;
-   private JButton button, button_1, button_2, button_3, button_4;
-   private JComboBox comboBox, comboBox_1;
+   private JButton registerBookButton, modifyButton, cancelButton, searchButton, deleteButton;
+   private JComboBox genreBox, categoryBox;
    private String[] index = {"전체", "제목", "저자", "출판사", "장르"};
    private List<String> genre;
    private JScrollPane scrollPane;
@@ -80,15 +81,15 @@ public class RetrieveBookPanel extends JPanel implements ActionListener {
 	};
 
 	class ButtonEditor extends DefaultCellEditor {
-		protected JButton button;
+		protected JButton registerBookButton;
 		private String label;
 		private boolean isPushed;
 
 		public ButtonEditor(JCheckBox checkBox) {
 			super(checkBox);
-			button = new JButton();
-			button.setOpaque(true);
-			button.addActionListener(new ActionListener() {
+			registerBookButton = new JButton();
+			registerBookButton.setOpaque(true);
+			registerBookButton.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					fireEditingStopped();
 				}
@@ -98,22 +99,22 @@ public class RetrieveBookPanel extends JPanel implements ActionListener {
 		public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row,
 				int column) {
 			if (isSelected) {
-				button.setForeground(table.getSelectionForeground());
-				button.setBackground(table.getSelectionBackground());
+				registerBookButton.setForeground(table.getSelectionForeground());
+				registerBookButton.setBackground(table.getSelectionBackground());
 			} else {
-				button.setForeground(table.getForeground());
-				button.setBackground(table.getBackground());
+				registerBookButton.setForeground(table.getForeground());
+				registerBookButton.setBackground(table.getBackground());
 			}
 			label = (value == null) ? "수정" : value.toString();
-			button.setText(label);
+			registerBookButton.setText(label);
 			isPushed = true;
-			return button;
+			return registerBookButton;
 		}
 
 		public Object getCellEditorValue() {
 			if (isPushed) {
 				int index = table.getSelectedRow();
-				JOptionPane.showMessageDialog(button, index + ": Ouch!");
+				JOptionPane.showMessageDialog(registerBookButton, index + ": Ouch!");
 			}
 			isPushed = false;
 			return new String(label);
@@ -134,11 +135,11 @@ public class RetrieveBookPanel extends JPanel implements ActionListener {
 		   try {
 			   Object target = e.getSource();
 			   
-			   if(target == button) {		// 도서 등록 화면 이동
+			   if(target == registerBookButton) {		// 도서 등록 화면 이동
 
 			   }
 			   
-			   else if(target == button_1) {		// 수정		1. ISBN 중복 -> ERROR   2. 날짜 오류 -> ERROR
+			   else if(target == modifyButton) {		// 수정		1. ISBN 중복 -> ERROR   2. 날짜 오류 -> ERROR
 				   
 				   Vector<Vector<Object>> book = new Vector<Vector<Object>>();
 				   BookDAO dao = new BookDAO();
@@ -146,36 +147,36 @@ public class RetrieveBookPanel extends JPanel implements ActionListener {
 				   book = dao.selectBookById((String)dm.getValueAt(row, 5));
 				   
 				   //String bookId, String subject, String writer, String publisher, String publishDate, String isbn, String isRent, int genre1
-				   BookVO mbook = new BookVO((String)book.get(0).get(0), textField.getText(),
-			                  textField_1.getText(), textField_2.getText(), textField_3.getText(), textField_4.getText(), "0", comboBox.getSelectedIndex()+1);
+				   BookVO mbook = new BookVO((String)book.get(0).get(0), subjectTF.getText(),
+			                  writerTF.getText(), publisherTF.getText(), publishDateTF.getText(), isbnTF.getText(), "0", genreBox.getSelectedIndex()+1);
 				   dao.updateBook(mbook);
 				   
 			   }
 			   
-			   else if(target == button_2) {		// 취소
+			   else if(target == cancelButton) {		// 취소
 				   
 				   Vector<Vector<Object>> book = new Vector<Vector<Object>>();
 				   BookDAO dao = new BookDAO();
 				   book = dao.selectBookById((String)dm.getValueAt(row, 5));
 				   
 				   BookVO mbook = new BookVO((String)book.get(0).get(0), (String)book.get(0).get(1),
-						   (String)book.get(0).get(2), (String)book.get(0).get(3), ((String)book.get(0).get(6)).substring(0, 10), (String)book.get(0).get(4), "0", comboBox.getSelectedIndex()+1);
+						   (String)book.get(0).get(2), (String)book.get(0).get(3), ((String)book.get(0).get(6)).substring(0, 10), (String)book.get(0).get(4), "0", genreBox.getSelectedIndex()+1);
 				   
-				   textField.setText(mbook.getSubject());
-				   textField_1.setText(mbook.getWriter());
-				   textField_2.setText(mbook.getPublisher());
-				   textField_3.setText(mbook.getPublishDate());
-				   textField_4.setText(mbook.getIsbn());
-				   comboBox.setSelectedIndex(mbook.getGenre1()-1);
+				   subjectTF.setText(mbook.getSubject());
+				   writerTF.setText(mbook.getWriter());
+				   publisherTF.setText(mbook.getPublisher());
+				   publishDateTF.setText(mbook.getPublishDate());
+				   isbnTF.setText(mbook.getIsbn());
+				   genreBox.setSelectedIndex(mbook.getGenre1()-1);
 				   
 			   }
-			   else if(target == button_3) {		// 검색
+			   else if(target == searchButton) {		// 검색
 				   dm.setRowCount(0);		// 새로 검색할때마다 0으로
 				   int cnt = 1;
 				   BookDAO dao = new BookDAO();
-				   Vector<Vector<Object>> lists = dao.selectBookList((int)comboBox_1.getSelectedIndex()+1, textField_5.getText());
+				   Vector<Vector<Object>> lists = dao.selectBookList((int)categoryBox.getSelectedIndex()+1, textTF.getText());
 				   
-				   if((int)comboBox_1.getSelectedIndex() != 0 && textField_5.getText().equals("")) {
+				   if((int)categoryBox.getSelectedIndex() != 0 && textTF.getText().equals("")) {
 					   JOptionPane.showMessageDialog(this, "검색어를 입력해주세요.");
 					   return;
 				   }
@@ -199,12 +200,14 @@ public class RetrieveBookPanel extends JPanel implements ActionListener {
 					   
 				   }
 			   }
-			   else if(target == button_4) {			// 삭제
+			   else if(target == deleteButton) {			// 삭제
+				   List<String> idList = new ArrayList<String>();
 				   for(int i = 0; i < dm.getRowCount(); i++) {
 						System.out.println("i번째: "+ dm.getValueAt(i, 0));
 						if((boolean)dm.getValueAt(i, 0) == true) {
-							//idList.add((String)dtm.getValueAt(i, 1));
-							//deleteBookList(idList);
+							idList.add((String)dm.getValueAt(i, 1));
+							BookDAO dao = new BookDAO();
+							//dao.deleteBookList(idList);
 							//String selectedId = (String) dtm.getValueAt(index, 1);	// 해당 칼럼의 id
 						}
 					}
@@ -227,12 +230,12 @@ public class RetrieveBookPanel extends JPanel implements ActionListener {
 	  			
 	  			for(int i=0; i<book.size(); i++) {
 	  				//book_id, title, writer, publisher, isbn, status, publish_date, genre_id
-	  				textField.setText((String)dm.getValueAt(row, 1));
-	  				textField_2.setText((String)dm.getValueAt(row, 3));
-	  				textField_1.setText((String)dm.getValueAt(row, 2));
-	  				comboBox.setSelectedIndex((int)book.get(i).get(7)-1);
-	  				textField_4.setText((String)dm.getValueAt(row, 5));
-	  				textField_3.setText(((String)book.get(i).get(6)).substring(0, 10));
+	  				subjectTF.setText((String)dm.getValueAt(row, 1));
+	  				publisherTF.setText((String)dm.getValueAt(row, 3));
+	  				writerTF.setText((String)dm.getValueAt(row, 2));
+	  				genreBox.setSelectedIndex((int)book.get(i).get(7)-1);
+	  				isbnTF.setText((String)dm.getValueAt(row, 5));
+	  				publishDateTF.setText(((String)book.get(i).get(6)).substring(0, 10));
 	  				spinner.setValue(book.size());
 	  			}
 	  			
@@ -244,10 +247,10 @@ public class RetrieveBookPanel extends JPanel implements ActionListener {
    };
 	
 	private void addEventListener() {
-		button.addActionListener(this);
-		button_1.addActionListener(this);
-		button_2.addActionListener(this);
-		button_3.addActionListener(this);
+		registerBookButton.addActionListener(this);
+		modifyButton.addActionListener(this);
+		cancelButton.addActionListener(this);
+		searchButton.addActionListener(this);
 		table.addMouseListener(listener);
 	}
 	
@@ -261,108 +264,115 @@ public class RetrieveBookPanel extends JPanel implements ActionListener {
 	      add(panel_1);
 	      panel_1.setLayout(null);
 	      
-	      JTextPane textPane = new JTextPane();
-	      textPane.setBackground(SystemColor.menu);
-	      textPane.setText("\uC81C\uBAA9");
-	      textPane.setBounds(26, 89, 47, 21);
-	      panel_1.add(textPane);
+	      JTextPane subject = new JTextPane();
+	      subject.setEditable(false);
+	      subject.setBackground(SystemColor.menu);
+	      subject.setText("\uC81C\uBAA9");
+	      subject.setBounds(26, 89, 47, 21);
+	      panel_1.add(subject);
 	      
-	      JTextPane textPane_1 = new JTextPane();
-	      textPane_1.setBackground(SystemColor.menu);
-	      textPane_1.setText("\uC800\uC790");
-	      textPane_1.setBounds(26, 146, 47, 21);
-	      panel_1.add(textPane_1);
+	      JTextPane writer = new JTextPane();
+	      writer.setEditable(false);
+	      writer.setBackground(SystemColor.menu);
+	      writer.setText("\uC800\uC790");
+	      writer.setBounds(26, 146, 47, 21);
+	      panel_1.add(writer);
 	      
-	      JTextPane textPane_2 = new JTextPane();
-	      textPane_2.setBackground(SystemColor.menu);
-	      textPane_2.setText("\uC7A5\uB974");
-	      textPane_2.setBounds(27, 200, 47, 21);
-	      panel_1.add(textPane_2);
+	      JTextPane genre_TP = new JTextPane();
+	      genre_TP.setEditable(false);
+	      genre_TP.setBackground(SystemColor.menu);
+	      genre_TP.setText("\uC7A5\uB974");
+	      genre_TP.setBounds(27, 200, 47, 21);
+	      panel_1.add(genre_TP);
 	      
-	      textField = new JTextField("");
-	      textField.setBounds(73, 89, 230, 21);
-	      panel_1.add(textField);
-	      textField.setColumns(10);
-	      
-	      comboBox = new JComboBox();
+	      genreBox = new JComboBox();
 	      BookDAO dao = new BookDAO();
 	      List<GenreVO> genres = dao.retrieveGenreList();
 	      Vector<String> genre = new Vector<String>();
 	      for(int i=0; i<genres.size();i++)
 	      	genre.add(genres.get(i).getGenre_name());
-	      comboBox.setModel(new DefaultComboBoxModel(genre));
-	      comboBox.setBounds(74, 200, 120, 21);
-	      panel_1.add(comboBox);			// 장르 검색해서 출력
 	      
-	      JTextPane textPane_3 = new JTextPane();
-	      textPane_3.setBackground(SystemColor.menu);
-	      textPane_3.setText("\uC218\uB7C9");
-	      textPane_3.setBounds(718, 89, 47, 21);
-	      panel_1.add(textPane_3);
+	      subjectTF = new JTextField("");
+	      subjectTF.setBounds(73, 89, 230, 21);
+	      panel_1.add(subjectTF);
+	      subjectTF.setColumns(10);
+	      genreBox.setModel(new DefaultComboBoxModel(genre));
+	      genreBox.setBounds(74, 200, 120, 21);
+	      panel_1.add(genreBox);			// 장르 검색해서 출력
+	      
+	      JTextPane quantity = new JTextPane();
+	      quantity.setEditable(false);
+	      quantity.setBackground(SystemColor.menu);
+	      quantity.setText("\uC218\uB7C9");
+	      quantity.setBounds(718, 89, 47, 21);
+	      panel_1.add(quantity);
 	      
 	      spinner = new JSpinner();
 	      spinner.setEnabled(false);
-	      spinner.setModel(new SpinnerNumberModel(new Integer(0), new Integer(0), null, new Integer(1)));
+	      spinner.setModel(new SpinnerNumberModel(new Integer(1), new Integer(1), null, new Integer(1)));
 	      spinner.setBounds(765, 89, 66, 21);
 	      panel_1.add(spinner);
 	      
-	      JTextPane textPane_4 = new JTextPane();
-	      textPane_4.setBackground(SystemColor.menu);
-	      textPane_4.setText("\uCD9C\uD310\uC0AC");
-	      textPane_4.setBounds(342, 89, 47, 21);
-	      panel_1.add(textPane_4);
+	      JTextPane publisher = new JTextPane();
+	      publisher.setEditable(false);
+	      publisher.setBackground(SystemColor.menu);
+	      publisher.setText("\uCD9C\uD310\uC0AC");
+	      publisher.setBounds(342, 89, 47, 21);
+	      panel_1.add(publisher);
 	      
-	      JTextPane textPane_5 = new JTextPane();
-	      textPane_5.setBackground(SystemColor.menu);
-	      textPane_5.setText("\uCD9C\uD310\uC77C");
-	      textPane_5.setBounds(342, 146, 47, 21);
-	      panel_1.add(textPane_5);
+	      JTextPane publishDate = new JTextPane();
+	      publishDate.setEditable(false);
+	      publishDate.setBackground(SystemColor.menu);
+	      publishDate.setText("\uCD9C\uD310\uC77C");
+	      publishDate.setBounds(342, 146, 47, 21);
+	      panel_1.add(publishDate);
 	      
-	      JTextPane txtpnIsbn = new JTextPane();
-	      txtpnIsbn.setBackground(SystemColor.menu);
-	      txtpnIsbn.setText("ISBN");
-	      txtpnIsbn.setBounds(342, 200, 47, 21);
-	      panel_1.add(txtpnIsbn);
+	      JTextPane isbn = new JTextPane();
+	      isbn.setEditable(false);
+	      isbn.setBackground(SystemColor.menu);
+	      isbn.setText("ISBN");
+	      isbn.setBounds(342, 200, 47, 21);
+	      panel_1.add(isbn);
 	      
-	      button = new JButton("도서등록");
-	      button.setBounds(830, 30, 90, 40);
-	      panel_1.add(button);
+	      registerBookButton = new JButton("도서등록");
+	      registerBookButton.setBounds(830, 30, 90, 40);
+	      panel_1.add(registerBookButton);
 	      
-	      button_1 = new JButton("\uC218    \uC815");
-	      button_1.addActionListener(new ActionListener() {
+	      modifyButton = new JButton("\uC218    \uC815");
+	      modifyButton.addActionListener(new ActionListener() {
 	      	public void actionPerformed(ActionEvent e) {
 	      	}
 	      });
-	      button_1.setBounds(718, 190, 90, 40);
-	      panel_1.add(button_1);
+	      modifyButton.setBounds(718, 190, 90, 40);
+	      panel_1.add(modifyButton);
 	      
-	      button_2 = new JButton("\uCDE8    \uC18C");
-	      button_2.addActionListener(new ActionListener() {
+	      cancelButton = new JButton("\uCDE8    \uC18C");
+	      cancelButton.addActionListener(new ActionListener() {
 	      	public void actionPerformed(ActionEvent e) {
 	      	}
 	      });
-	      button_2.setBounds(830, 190, 90, 40);
-	      panel_1.add(button_2);
+	      cancelButton.setBounds(830, 190, 90, 40);
+	      panel_1.add(cancelButton);
 	      
-	      textField_1 = new JTextField("");
-	      textField_1.setColumns(10);
-	      textField_1.setBounds(73, 146, 230, 21);
-	      panel_1.add(textField_1);
+	      writerTF = new JTextField("");
+	      writerTF.setColumns(10);
+	      writerTF.setBounds(73, 146, 230, 21);
+	      panel_1.add(writerTF);
 	      
-	      textField_2 = new JTextField("");
-	      textField_2.setColumns(10);
-	      textField_2.setBounds(401, 89, 287, 21);
-	      panel_1.add(textField_2);
+	      publisherTF = new JTextField("");
+	      publisherTF.setColumns(10);
+	      publisherTF.setBounds(401, 89, 287, 21);
+	      panel_1.add(publisherTF);
 	      
-	      textField_3 = new JTextField("");
-	      textField_3.setColumns(10);
-	      textField_3.setBounds(401, 146, 287, 21);
-	      panel_1.add(textField_3);
+	      publishDateTF = new JTextField("");
+	      publishDateTF.setColumns(10);
+	      publishDateTF.setBounds(401, 146, 287, 21);
+	      panel_1.add(publishDateTF);
 	      
-	      textField_4 = new JTextField("");
-	      textField_4.setColumns(10);
-	      textField_4.setBounds(401, 200, 287, 21);
-	      panel_1.add(textField_4);
+	      isbnTF = new JTextField("");
+	      isbnTF.setColumns(10);
+	      isbnTF.setBounds(401, 200, 287, 21);
+	      panel_1.add(isbnTF);
 	      
 	      scrollPane = new JScrollPane();
 	      scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
@@ -410,23 +420,23 @@ public class RetrieveBookPanel extends JPanel implements ActionListener {
 	      table.getColumnModel().getColumn(5).setPreferredWidth(200);
 	      scrollPane.setViewportView(table);
 	      
-	      comboBox_1 = new JComboBox();
-	      comboBox_1.setModel(new DefaultComboBoxModel(index));
-	      comboBox_1.setBounds(26, 40, 70, 21);
-	      panel_1.add(comboBox_1);
+	      categoryBox = new JComboBox();
+	      categoryBox.setModel(new DefaultComboBoxModel(index));
+	      categoryBox.setBounds(26, 40, 70, 21);
+	      panel_1.add(categoryBox);
 	      
-	      textField_5 = new JTextField("");		// 책 검색 필드
-	      textField_5.setColumns(10);
-	      textField_5.setBounds(108, 40, 287, 21);
-	      panel_1.add(textField_5);
+	      textTF = new JTextField("");		// 책 검색 필드
+	      textTF.setColumns(10);
+	      textTF.setBounds(108, 40, 287, 21);
+	      panel_1.add(textTF);
 	      
-	      button_3 = new JButton("\uAC80\uC0C9");
-	      button_3.setBounds(407, 39, 70, 23);
-	      panel_1.add(button_3);
+	      searchButton = new JButton("\uAC80\uC0C9");
+	      searchButton.setBounds(407, 39, 70, 23);
+	      panel_1.add(searchButton);
 	      
-	      button_4 = new JButton("선택 도서 삭제");
-	      button_4.setBounds(834, 715, 124, 35);
-	      panel_1.add(button_4);
+	      deleteButton = new JButton("선택 도서 삭제");
+	      deleteButton.setBounds(834, 715, 124, 35);
+	      panel_1.add(deleteButton);
 	      
 	      
 		}

31835afa0ff65dbeab4f3fee443faa936e2032dd
HikingBe4r <kimisa0927@gmail.com>
2017-04-20 11:45:04 +0900

    생일 입력 수정

--- src/ui/MemberPanel.java
+++ src/ui/MemberPanel.java
@@ -493,7 +493,7 @@ public class MemberPanel extends JPanel {
 
 	private int checkTF() {
 		String phone_ptn = "^01(?:\\d)-(?:\\d{3}|\\d{4})-\\d{4}$"; // 전화번호
-		String bday_ptn = "^(18[0-9][0-9]|19[0-9][0-9]|20\\d{2})(\\/|-)(0[0-9]|1[0-2])(\\/|-)(0[1-9]|[1-2][0-9]|3[0-1])$"; // 생일
+		String bday_ptn = "^(18[0-9][0-9]|19[0-9][0-9]|20\\d{2})(\\/|-|.)(0[0-9]|1[0-2])(\\/|-|.)(0[1-9]|[1-2][0-9]|3[0-1])$"; // 생일
 
 		Pattern phone = Pattern.compile(phone_ptn);
 		Matcher phoneMatch = phone.matcher(phoneTF.getText());
